a54b399fffa026a924f562531b4480c6ce730562
AndrewM87 <andrewmontgomery87@icloud.com>
2024-08-26 14:25:08 +0700

    Hello world file for CSC101

---
+++ Helloworld.filesubmission.py
@@ -0,0 +1,8 @@
+# Program Title: A Few Things About Me
+# Author: Andrew Montgomery
+# Description: Here are some details about me in the Python programming language.
+
+print("Hello, World!")
+print("""My name is Andrew Montgomery. 
+I'm from Mississippi but live in Thailand with my wife and two daughters. 
+I enjoy jiu-jitsu and making things.""")
